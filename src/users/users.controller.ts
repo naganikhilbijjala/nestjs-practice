4195e2b270cbcaea70e933cd2fd006089662b967
Nikhil Bijjala <nbijjala@smart-structures.com>
2025-10-26 18:55:19 -0400

    feat: add user service and update corresponding methods in the controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,41 +7,60 @@ import {
   Patch,
   Post,
   Query,
-} from '@nestjs/common';
+} from "@nestjs/common";
+import { UsersService } from "./users.service";
 
-@Controller('users')
+@Controller("users")
 export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
   // Using Query Params
   // /users?role=value
   @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
+  findAll(@Query("role") role?: "INTERN" | "ENGINEER" | "ADMIN") {
+    return this.usersService.findAll(role);
   }
 
   //We should keep interns route above because otherwise it will consider
   // this as id param
-  @Get('interns')
+  @Get("interns")
   findAllInterns() {
     return [];
   }
 
-  @Get('/:id')
-  findOne(@Param('id') id: string) {
-    return { id };
+  @Get("/:id")
+  findOne(@Param("id") id: string) {
+    // Converts to number if it is not a number already
+    // Params are by default strings in Nest.js
+    return this.usersService.findOne(+id);
   }
 
   @Post()
-  create(@Body() user: {}) {
-    return user;
+  create(
+    @Body()
+    user: {
+      name: string;
+      email: string;
+      role: "INTERN" | "ENGINEER" | "ADMIN";
+    }
+  ) {
+    return this.usersService.create(user);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
+  @Patch(":id")
+  update(
+    @Param("id") id: string,
+    @Body()
+    userUpdate: {
+      name?: string;
+      email?: string;
+      role?: "INTERN" | "ENGINEER" | "ADMIN";
+    }
+  ) {
+    return this.usersService.update(+id, userUpdate);
   }
 
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id };
+  @Delete(":id")
+  delete(@Param("id") id: string) {
+    return this.usersService.delete(+id);
   }
 }
