a7b889c4af5c11a8c780a7f4e64c46cf3e95ab91
Nikhil Bijjala <nbijjala@smart-structures.com>
2025-10-27 09:26:28 -0400

    feat: add user service to add DTO's

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from "@nestjs/common";
+import { UpdateUserDto } from "./dto/update-user.dto";
+import { CreateUserDto } from "./dto/create-user.dto";
+import { NotFoundException } from "@nestjs/common";
 
 @Injectable()
 export class UsersService {
@@ -44,34 +47,26 @@ export class UsersService {
 
   findOne(id: number) {
     const user = this.users.find((user) => user.id === id);
+    if (!user) {
+      throw new NotFoundException("User Not Found");
+    }
     return user;
   }
 
-  create(user: {
-    name: string;
-    email: string;
-    role: "INTERN" | "ENGINEER" | "ADMIN";
-  }) {
+  create(createUserDto: CreateUserDto) {
     const usersByHighestId = [...this.users].sort((a, b) => b.id - a.id);
     const newUser = {
       id: usersByHighestId[0].id + 1,
-      ...user,
+      ...createUserDto,
     };
     this.users.push(newUser);
     return newUser;
   }
 
-  update(
-    id: number,
-    updatedUser: {
-      name?: string;
-      email?: string;
-      role?: "INTERN" | "ENGINEER" | "ADMIN";
-    }
-  ) {
+  update(id: number, updateUserDto: UpdateUserDto) {
     this.users = this.users.map((user) => {
       if (user.id === id) {
-        return { ...user, ...updatedUser };
+        return { ...user, ...updateUserDto };
       }
       return user;
     });
